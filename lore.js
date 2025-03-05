142ad3b620dcd93f7452aae2e56861bbdabfb78c
Jack Hope <jackhoppejj@gmail.com>
2025-03-04 22:47:04 -0600

    Implemented Firestore authentication to acess database

--- lore.js
+++ lore.js
@@ -36,4 +36,42 @@ function installAddon() {
   onOpen();
 }
 
+//Important information for firebase API to connect with the database
+const PROJECT_ID = "howdy-ed4fe";
+const FIREBASE_URL = `https://firestore.googleapis.com/v1/projects/${PROJECT_ID}/databases/(default)/documents/`;
 
+const SERVICE_ACCOUNT_EMAIL = "firebase-adminsdk-fbsvc@howdy-ed4fe.iam.gserviceaccount.com";
+const PRIVATE_KEY = getFirestorePrivateKey().replace(/\\n/g, '\n');
+
+//Get authentication token using Google Apps Script's OAuth service
+function getAccessToken() {
+  var jwt = {
+    alg: "RS256",
+    typ: "JWT"
+  };
+
+  var claimSet = {
+    iss: SERVICE_ACCOUNT_EMAIL,
+    scope: "https://www.googleapis.com/auth/datastore",
+    aud: "https://oauth2.googleapis.com/token",
+    exp: Math.floor(Date.now() / 1000) + 3600,
+    iat: Math.floor(Date.now() / 1000)
+  };
+
+  var header = Utilities.base64EncodeWebSafe(JSON.stringify(jwt));
+  var claim = Utilities.base64EncodeWebSafe(JSON.stringify(claimSet));
+  var signature = Utilities.computeRsaSha256Signature(header + "." + claim, PRIVATE_KEY);
+  var signedJwt = header + "." + claim + "." + Utilities.base64EncodeWebSafe(signature);
+
+  var options = {
+    method: "post",
+    payload: {
+      grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
+      assertion: signedJwt
+    }
+  };
+
+  var response = UrlFetchApp.fetch("https://oauth2.googleapis.com/token", options);
+  var json = JSON.parse(response.getContentText());
+  return json.access_token;
+}
