4ab997dcfeef3f7ab3a81dbd2cc5d00b26b7127d
sevanisk <112203343+sevanisk@users.noreply.github.com>
2025-04-01 03:03:56 -0500

    added placeholders for setting functions

--- variable.js
+++ variable.js
@@ -18,3 +18,21 @@ function getOpenCount() {
   var count = parseInt(props.getProperty('open_count') || "0", 10);
   return count;
 }
+
+function getBoundDocumentURL() {
+  var doc = DocumentApp.getActiveDocument();
+  if (doc) {
+      Logger.log("Bound Document URL: " + doc.getUrl());
+  } else {
+      Logger.log("No document is bound to this script.");
+  }
+}
+
+function getActiveName() {
+  var doc = DocumentApp.getActiveDocument();
+  if (doc) {
+      Logger.log("Document Name: " + doc.getName());
+  } else {
+      Logger.log("No document is bound to this script.");
+  }
+}
